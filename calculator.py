3acfb0b69d677cde09f307233184e1aedf5d1206
Fadhil Anwar <158454024+fadhilanwar@users.noreply.github.com>
2024-11-05 14:03:18 +0700

    Menambahkan calculator.py

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def tambah(x, y):
+    return x + y
+def kurang(x, y):
+    return x - y
+def kali(x, y):
+    return x * y
+def bagi(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Pembagi harus > 0!"
+
+print("Pilih Salah satu oprasi:")
+print("1. Tambah")
+print("2. Kurang")
+print("3. Kali")
+print("4. Bagi")
+
+pilihan = input("Masukkan IPilihan: ")
+
+# Meminta input angka pertama dan kedua
+angka1 = float(input("Masukkan angka: "))
+angka2 = float(input("Masukkan angka kedua: "))
+
+# Melakukan operasi berdasarkan pilihan
+if pilihan == '1':
+    print(f"{angka1} + {angka2} = {tambah(angka1, angka2)}")
+elif pilihan == '2':
+    print(f"{angka1} - {angka2} = {kurang(angka1, angka2)}")
+elif pilihan == '3':
+    print(f"{angka1} * {angka2} = {kali(angka1, angka2)}")
+elif pilihan == '4':
+    print(f"{angka1} / {angka2} = {bagi(angka1, angka2)}")
+else:
+    print("Pilihan tidak sesuai!")
